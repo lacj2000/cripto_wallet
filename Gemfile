5dbe5810c421445fb81f3b388bb1f51b4434cdc7
lacj2000 <luizjuniordeveloper@gmail.com>
2023-02-01 14:59:23 -0300

    add bootstrap and popperjs

--- Gemfile
+++ Gemfile
@@ -48,6 +48,13 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem 'rails-i18n', '~> 7.0', '>= 7.0.6'
+
+gem 'bootstrap', '>=5.2.3'
+
+
+
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # Undefault usage for multiples files in path
 # Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/assets/javascript/*"].sort_by { |dir| -dir.size }
-# Rails.application.config.assets.precompile += %w( jquery-3.6.3.js notify.js )
+Rails.application.config.assets.precompile += %w( jquery-3.6.3.js notify.js )
+Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
