76361d3e3af83274064c2645f0447e1c2f2fa4bf
lacj2000 <luizjuniordeveloper@gmail.com>
2023-01-31 22:41:20 -0300

    add libraries using vendor - bootstrap notifyjs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Undefault usage for multiples files in path
+# Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/assets/javascript/*"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.precompile += %w( jquery-3.6.3.js notify.js )
