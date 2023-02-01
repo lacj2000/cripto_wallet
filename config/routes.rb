9d7af2b6d7113e9de2df62a5f342522290c8bb42
lacj2000 <luizjuniordeveloper@gmail.com>
2023-02-01 20:59:59 -0300

    welcome page - i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "welcome/index"
+  # get "welcome/index"
   root to: "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
